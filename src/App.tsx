1ea55f5a3bd7dc6edc6a0abe09e51c826a99b4bb
yuizho <yuizho3@gmail.com>
2020-05-30 23:15:53 +0900

    rewrite with Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,42 @@
-import React, { Component } from "react";
+import React, { FC, useState } from "react";
 import { Button, Card, Statistic } from "semantic-ui-react";
 
 import "./App.css";
 
-interface AppState {
-  count: number;
-}
+const App: FC = () => {
+  const [count, setCount] = useState(0);
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
-  increment = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
+  const increment = () => {
+    setCount(count + 1);
   };
 
-  decrement = () => {
-    this.setState((prevState) => ({
-      count: prevState.count - 1,
-    }));
+  const decrement = () => {
+    setCount(count - 1);
   };
 
-  render() {
-    const { count } = this.state;
-    return (
-      <div className="counter">
-        <header>
-          <h1>counter</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <div className="ui two buttons">
-              <Button color="red" onClick={this.decrement}>
-                -1
-              </Button>
-              <Button color="green" onClick={this.increment}>
-                +1
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="counter">
+      <header>
+        <h1>counter</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={decrement}>
+              -1
+            </Button>
+            <Button color="green" onClick={increment}>
+              +1
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default App;
